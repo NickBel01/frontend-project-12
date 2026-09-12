7bfc66ee09d9acaafbb94e962c4b02cb53c777bb
Nikita Belyaev <n.nikitin8888@gmail.com>
2026-09-13 00:47:34 +0300

    fix: rename init.js to init.jsx for JSX support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,3 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { initSentry } from './sentry.js';
+import init from '../../src/init.jsx';
 
-export const runApp = () => {
-  initSentry();
-
-  let container = document.getElementById('root');
-  if (!container) {
-    container = document.createElement('div');
-    container.id = 'root';
-    document.body.appendChild(container);
-  }
-
-  createRoot(container).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-};
-
-export default runApp;
+init();
