95a780e96c6e9f08a53acfef1c04832ee516c717
Nikita Belyaev <n.nikitin8888@gmail.com>
2026-09-17 11:58:40 +0300

    fix: reset auth store before init

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,10 +1,12 @@
 import { StrictMode, createElement } from 'react';
 import { createRoot } from 'react-dom/client';
+import authStore from './store/auth.js';
 import App from './app/App.jsx';
 
 let root = null;
 
 export default function init() {
+  authStore.getState().removeAuth();
   localStorage.clear();
 
   let container = document.getElementById('root');
